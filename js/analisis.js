ae8c1f3847d94071296c68c75ea5212e72328f86
teo009 <teo.obando09@gmail.com>
2022-01-19 16:01:42 -0600

    Ordenando salarios, calculando media, mediana y imprimiento valor en consola

--- js/analisis.js
+++ js/analisis.js
@@ -1,1 +1,46 @@
 //Proceso de cálculos
+
+//Array que tenga solo los salarios
+const salariosAr = argentina.map(
+    function(persona) {
+        return persona.salary
+    }
+)
+
+//Salarios ordenados
+const salariosArSorted = salariosAr.sort(
+    function(salaryA, salaryB) {
+       return (salaryA - salaryB )
+    }
+)
+
+function esPar(numero) {
+    return (numero % 2 === 0) 
+}
+
+function mediaSalarios(lista) {
+    const sumaLista = lista.reduce(
+        function(valorAcomulado=0, nuevoElemento) {
+            return (valorAcomulado + nuevoElemento)
+        }
+    )
+    const promedioLista = sumaLista / lista.length
+    return promedioLista
+}
+
+function medianaSalarios(lista) {
+    const mitad = parseInt(lista.length / 2)
+    if(esPar(lista.length)) {
+        const personaMitad1 = lista[mitad - 1]
+        const personaMitad2 = lista[mitad]
+        const mediana = mediaSalarios([personaMitad1, personaMitad2])
+        return mediana
+    } else {
+        const personaMitad = lista[mitad]
+        return personaMitad
+    }
+}
+
+console.log(
+    medianaSalarios(salariosArSorted)
+)
--- js/salarios.js
+++ js/salarios.js
@@ -1,1 +1,14 @@
 //Lista de salarios
+
+const argentina = []
+
+argentina.push({name: 'Teo', salary: 500})
+argentina.push({name: 'Jeff', salary: 600})
+argentina.push({name: 'Dan', salary: 700})
+argentina.push({name: 'Isra', salary: 200})
+argentina.push({name: 'Lucy', salary: 900})
+argentina.push({name: 'Key', salary: 450})
+argentina.push({name: 'Leo', salary: 100})
+argentina.push({name: 'Ian', salary: 720})
+argentina.push({name: 'Less', salary: 630})
+argentina.push({name: 'Ken', salary: 900})
