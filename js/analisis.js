3e5a31466006847347dac85aefc59a8dd95c84ce
teo009 <teo.obando09@gmail.com>
2022-02-01 15:07:13 -0600

    Calculando mediana del top 10 de salarios, usando el metodo splice

--- js/analisis.js
+++ js/analisis.js
@@ -1,31 +1,6 @@
-//Proceso de cálculos
-
-//Array que tenga solo los salarios
-const salariosAr = argentina.map(
-    function(persona) {
-        return persona.salary
-    }
-)
-
-//Salarios ordenados
-const salariosArSorted = salariosAr.sort(
-    function(salaryA, salaryB) {
-       return (salaryA - salaryB )
-    }
-)
-
+//Función HELPERS
 function esPar(numero) {
     return (numero % 2 === 0) 
-}
-
-function mediaSalarios(lista) {
-    const sumaLista = lista.reduce(
-        function(valorAcomulado=0, nuevoElemento) {
-            return (valorAcomulado + nuevoElemento)
-        }
-    )
-    const promedioLista = sumaLista / lista.length
-    return promedioLista
 }
 
 function medianaSalarios(lista) {
@@ -41,6 +16,43 @@ function medianaSalarios(lista) {
     }
 }
 
-console.log(
-    medianaSalarios(salariosArSorted)
+//Array que tenga solo los salarios
+const salariosAr = argentina.map(
+    function(persona) {
+        return persona.salary
+    }
 )
+
+//Salarios ordenados
+const salariosArSorted = salariosAr.sort(
+    function(salaryA, salaryB) {
+       return (salaryA - salaryB )
+    }
+)
+
+function mediaSalarios(lista) {
+    const sumaLista = lista.reduce(
+        function(valorAcomulado=0, nuevoElemento) {
+            return (valorAcomulado + nuevoElemento)
+        }
+    )
+    const promedioLista = sumaLista / lista.length
+    return promedioLista
+}
+
+const medianaGeneralAr = medianaSalarios(salariosArSorted)
+
+//Mediana Top 10%
+const spliceStart = ((salariosArSorted.length * (100 - 10))/100)
+const spliceCount = salariosArSorted.length - spliceStart
+const salaryTop10 = salariosArSorted.splice(
+    spliceStart,
+    spliceCount,
+)
+const medianaTop10Ar = medianaSalarios(salaryTop10)
+
+console.log({
+    medianaGeneralAr,
+    medianaTop10Ar,
+    salaryTop10,
+})
